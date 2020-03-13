e355074f296325356ad1606323aca2a75eb78d53
himanikh <himani.arora78@gmail.com>
2020-03-13 13:14:11 -0700

    Update MissingPagination.java

--- MissingPagination.java
+++ MissingPagination.java
@@ -17,40 +17,5 @@ import java.util.List;
 import java.util.Map;
 
 public class MissingPagination {
-    private static CFNHelperSingleton instance = new CFNHelperSingleton();
-    private Map<String, AmazonCloudFormation> cfnPerRegionMap = new HashMap<>();
 
-    private CFNHelperSingleton() {
-    }
-
-    public static CFNHelperSingleton getInstance() {
-        return instance;
-    }
-
-    private AmazonCloudFormation getCfnClient(String region) {
-        return cfnPerRegionMap.computeIfAbsent(region, k -> AmazonCloudFormationClientBuilder.standard()
-                .withCredentials(new DefaultAWSCredentialsProviderChain())
-                .withRegion(region).build());
-    }
-
-    public Map<String, Object> getExistingStackParameters(String stackName, String region) throws IOException {
-        AmazonCloudFormation client = getCfnClient(region);
-        try {
-            GetTemplateResult result = client.getTemplate(new GetTemplateRequest().withStackName(stackName));
-            String template = result.getTemplateBody();
-            return new ObjectMapper(new YAMLFactory()).readValue(template, Map.class);
-        } catch (AmazonCloudFormationException e) {
-            if (e.getMessage().contains("does not exist")) {
-                return null;
-            } else {
-                throw e;
-            }
-        }
-    }
-
-    public List<StackSummary> listStacks(String region) {
-        AmazonCloudFormation client = getCfnClient(region);
-        ListStacksResult result = client.listStacks();
-        return result.getStackSummaries();
-    }
 }
